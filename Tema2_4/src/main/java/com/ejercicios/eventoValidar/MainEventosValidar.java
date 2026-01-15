246ae5c1ba984846477f72c95ce605f13434f432
HugoD24 <4048314@alu.murciaeduca.es>
2026-01-15 13:59:36 +0100

    avanzado nuevo proyecto

--- Tema2_4/src/main/java/com/ejercicios/eventoValidar/MainEventosValidar.java
+++ Tema2_4/src/main/java/com/ejercicios/eventoValidar/MainEventosValidar.java
@@ -1,4 +1,26 @@
 package com.ejercicios.eventoValidar;
 
-public class MainEventosValidar {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class MainEventosValidar extends Application {
+
+    private VBox rootLayout;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("EventosValidar.fxml"));
+        rootLayout = (VBox) loader.load();
+
+
+        Scene scene = new Scene(rootLayout);
+        stage.setTitle("Ejemplo sobre eventos!");
+        stage.setScene(scene);
+        stage.show();
+
+    }
 }
--- Tema2_4/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.tema2_4 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.tema2_4 to javafx.fxml;
-    exports org.example.tema2_4;
-}
