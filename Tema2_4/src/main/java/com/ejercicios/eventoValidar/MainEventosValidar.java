f83768d86b90b012533d44a60249963daa6b6e33
HugoD24 <4048314@alu.murciaeduca.es>
2026-01-22 12:43:31 +0100

    terminado EventosValidar

--- Tema2_4/src/main/java/com/ejercicios/eventoValidar/MainEventosValidar.java
+++ Tema2_4/src/main/java/com/ejercicios/eventoValidar/MainEventosValidar.java
@@ -2,25 +2,78 @@ package com.ejercicios.eventoValidar;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainEventosValidar extends Application {
 
     private VBox rootLayout;
 
     @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("EventosValidar.fxml"));
-        rootLayout = (VBox) loader.load();
+    public void start(Stage primaryStage) {
 
+        try {
 
-        Scene scene = new Scene(rootLayout);
-        stage.setTitle("Ejemplo sobre eventos!");
-        stage.setScene(scene);
-        stage.show();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainEventosValidar.class.getResource("EventosValidar.fxml"));
+            rootLayout = (VBox) loader.load();
 
+            Scene scene = new Scene(rootLayout);
+
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Ejemplo sobre eventos");
+            primaryStage.show();
+
+            crearDialogo(primaryStage, scene);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    private void crearDialogo(Stage primaryStage, Scene scene) {
+
+        TextInputDialog textDialog = new TextInputDialog("Texto de ejemplo...");
+        textDialog.initModality(Modality.WINDOW_MODAL);
+        textDialog.initOwner(primaryStage);
+        textDialog.setTitle("Ejemplo de dialogo");
+        textDialog.setHeaderText("Dialogo para introducir un texto");
+
+        textDialog.getEditor().setText("");
+        textDialog.getEditor().setPromptText("Texto de ejemplo");
+
+        Button btnMostrar = (Button) scene.lookup("#btnMostrar");
+        TextField txtResultado = (TextField) scene.lookup("#txtResultado");
+
+        btnMostrar.setOnAction(event -> {
+            textDialog.showAndWait().ifPresent(response -> {
+                txtResultado.setText(response);
+            });
+        });
+
+        textDialog.setOnHidden(event -> {
+            txtResultado.requestFocus();
+        });
+
+        txtResultado.setOnMouseClicked(event -> {
+            txtResultado.setCursor(Cursor.HAND);
+        });
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+
+
+
+
 }
---
+++ Tema2_4/src/main/java/draggableFiles/DragFilesController.java
@@ -0,0 +1,4 @@
+package draggableFiles;
+
+public class DragFilesController {
+}
